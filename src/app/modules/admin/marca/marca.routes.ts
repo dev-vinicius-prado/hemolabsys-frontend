c961cbaabbe9787a1f454e553bfb0b5dbf2cdce4
dev.vinnas <developer.vinicius.prado@gmail.com>
2026-03-14 16:11:00 -0300

    chore: remove marca and tipo-embalagem admin modules
    Delete the entire marca (brand) and tipo-embalagem (packaging type) admin modules, including components, styles, templates, and routing configurations. This cleanup removes unused or deprecated features from the codebase.

--- src/app/modules/admin/marca/marca.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { MarcaComponent } from './marca.component';
-
-export default [
-    {
-        path: '',
-        component: MarcaComponent,
-    },
-] as Routes;
--- src/app/modules/admin/tipo-embalagem/tipo-embalagem.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { TipoEmbalagemComponent } from './tipo-embalagem.component';
-
-export default [
-    {
-        path: '',
-        component: TipoEmbalagemComponent,
-    },
-] as Routes;
