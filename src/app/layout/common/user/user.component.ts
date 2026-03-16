994963fa58e00737ba7a071e92c231f6dd7213ff
dev.vinnas <developer.vinicius.prado@gmail.com>
2026-03-16 15:48:41 -0300

    feat(profile): add user profile page with internationalization support
    - Add new profile component with form for editing user information
    - Update user menu to use transloco for internationalization
    - Add profile route to application routing
    - Extend translation files with new profile-related strings
    - Fix avatar display logic in user component

--- src/app/layout/common/user/user.component.ts
+++ src/app/layout/common/user/user.component.ts
@@ -5,7 +5,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { TranslocoModule } from '@ngneat/transloco';
 import { UserService } from 'app/core/user/user.service';
 import { User } from 'app/core/user/user.types';
 import { Subject, takeUntil } from 'rxjs';
@@ -17,7 +18,7 @@ import { Subject, takeUntil } from 'rxjs';
     changeDetection: ChangeDetectionStrategy.OnPush,
     exportAs       : 'user',
     standalone     : true,
-    imports        : [MatButtonModule, MatMenuModule, NgIf, MatIconModule, NgClass, MatDividerModule],
+    imports        : [MatButtonModule, MatMenuModule, NgIf, MatIconModule, NgClass, MatDividerModule, TranslocoModule, RouterLink],
 })
 export class UserComponent implements OnInit, OnDestroy
 {
