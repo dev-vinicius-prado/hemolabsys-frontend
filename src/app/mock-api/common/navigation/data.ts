c85b49b19787838a21f24f2a79cb48e30a6e2c2a
dev.vinnas <developer.vinicius.prado@gmail.com>
2026-06-09 18:41:58 -0300

    chore(mock-api): remove insumos navigation menu item

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -10,13 +10,6 @@ export const defaultNavigation: FuseNavigationItem[] = [
         link: '/dashboard',
     },
     {
-        id: 'insumos',
-        title: 'Insumos',
-        type: 'basic',
-        icon: 'heroicons_outline:beaker',
-        link: '/insumos',
-    },
-    {
         id: 'entradas',
         title: 'Entradas',
         type: 'basic',
