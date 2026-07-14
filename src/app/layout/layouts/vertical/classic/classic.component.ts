0e2ab47ac3ce43aeb737e9e83094191041f16e65
dev.vinnas <developer.vinicius.prado@gmail.com>
2026-07-14 16:36:48 -0300

    Revert "ci: ajustes para o pipeline local"
    This reverts commit b9f898fe

--- src/app/layout/layouts/vertical/classic/classic.component.ts
+++ src/app/layout/layouts/vertical/classic/classic.component.ts
@@ -19,13 +19,14 @@ import { SearchComponent } from 'app/layout/common/search/search.component';
 import { ShortcutsComponent } from 'app/layout/common/shortcuts/shortcuts.component';
 import { UserComponent } from 'app/layout/common/user/user.component';
 import { combineLatest, Subject, takeUntil } from 'rxjs';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
     selector     : 'classic-layout',
     templateUrl  : './classic.component.html',
     encapsulation: ViewEncapsulation.None,
     standalone   : true,
-    imports      : [FuseLoadingBarComponent, FuseVerticalNavigationComponent, MatButtonModule, MatIconModule, LanguagesComponent, FuseFullscreenComponent, SearchComponent, ShortcutsComponent, MessagesComponent, NotificationsComponent, UserComponent, NgIf, RouterOutlet, QuickChatComponent],
+    imports      : [FuseLoadingBarComponent, FuseVerticalNavigationComponent, MatButtonModule, MatIconModule, LanguagesComponent, FuseFullscreenComponent, SearchComponent, NotificationsComponent, UserComponent, NgIf, RouterOutlet, MatTooltipModule],
 })
 export class ClassicLayoutComponent implements OnInit, OnDestroy
 {
